a5f3146e56c09f96a20073d2bcd061e9494bb6f7
PierrePinson <pierre.pinson@supelec.fr>
2020-02-06 13:22:43 +0100

    Simple features
    Concatenate radionomics and simple features in a simple dataframe

--- extract_features.py
+++ extract_features.py
@@ -1,1 +1,23 @@
+#path = 'C:/Users/Pierre Pinson/Documents/Cours/Supélec/3A/Stages/Owkin ML challenge/x_train/'
+import numpy as np
+import pandas as pd
 
+
+
+def extract_features(path):
+    clinical = pd.read_csv(path+'features/clinical_data.csv', sep=',')
+
+    features = (pd.read_csv(path+'features/radiomics.csv', sep=',', header = 1))
+    features['Adenocarcinoma'] = clinical.Histology.apply(lambda x: unsensitive_compare(x, "Adenocarcinoma"))
+    features['Large cell'] = clinical.Histology.apply(lambda x: unsensitive_compare(x, "large cell")) 
+    features['Squamous cell carcinoma'] = clinical.Histology.apply(lambda x: unsensitive_compare(x, "squamous cell carcinoma"))
+    features['NOS'] = clinical.Histology.apply(lambda x: unsensitive_compare(x, "nos")) | clinical.Histology.apply(lambda x: unsensitive_compare(x, "NSCLC NOS (not otherwise specified)")) 
+    features['age'] = clinical.age
+    features = features.drop("PatientID", axis=1)
+    return features
+
+def unsensitive_compare(str1, str2):
+    if type(str1) == str:
+        return int(str1.lower() == str2.lower())
+    else:
+        return 0
